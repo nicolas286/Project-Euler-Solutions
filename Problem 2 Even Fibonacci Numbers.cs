daf5c23f59939a0745c1b040f1bbd8f07e01dc13
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-01 20:56:35 +0100

    Add files via upload

---
+++ Problem 2 Even Fibonacci Numbers.cs
@@ -0,0 +1,18 @@
+﻿
+int valuesSum = 2;
+int firstValue = 1;
+int secondValue = 2;
+int currentValue = firstValue + secondValue;
+
+while (currentValue <= 4000000) {
+    if (currentValue % 2 == 0) {
+    valuesSum += currentValue; 
+    }
+    firstValue = secondValue;
+    secondValue = currentValue;
+    currentValue = firstValue + secondValue;
+};
+
+Console.WriteLine(valuesSum);
+Console.ReadKey();
+
