ce6a4db7890eb29350e73b3d763bd80c71e40549
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-02 15:55:18 +0100

    Add files via upload

---
+++ Problem 3 Largest Prime Factor.cs
@@ -0,0 +1,37 @@
+﻿
+long numberToFactorize = 600851475143;
+long answer = 0;
+
+for (long i = 2; i <= numberToFactorize / 2 ; i++) // we start loop from the bottom (faster)
+ {
+
+  if (numberToFactorize % i == 0) // if number is divisible by i
+    {
+
+      long oppositeDivider = numberToFactorize / i; // gets to larger divider
+
+      for(int primeChecker = 2; primeChecker <= (int)Math.Sqrt(oppositeDivider); primeChecker++)  // loop to check if larger divider is prime
+      {
+        if ((oppositeDivider % primeChecker) == 0) // break the loop as soon as a divider is found, which mean it's not a prime number
+        {
+         break;
+        }
+
+        if (primeChecker == (int)Math.Sqrt(oppositeDivider)) // if gets to the end of the loop, it means larger divider is prime : it is our answer
+        {
+         answer = oppositeDivider;
+         goto Found;
+        }
+
+      }
+    }    
+}
+Found:
+  Console.WriteLine(answer);   
+
+
+
+
+
+
+
