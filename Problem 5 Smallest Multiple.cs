e4b0c63a5d95638e5135c26be52999f64f175bc2
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-03 22:37:53 +0100

    Add files via upload

---
+++ Problem 5 Smallest Multiple.cs
@@ -0,0 +1,28 @@
+﻿bool answerFound = false;
+int answer = 0;
+int testNumber = 20;
+
+while (answerFound == false) {
+
+    for (int divider = 19; divider >= 3; divider--)
+    {
+
+        if (divider == 4 || divider == 5 || divider == 10) {
+            continue;
+        }
+
+        if ((testNumber % divider) != 0) {
+            break;
+        }
+
+        if (divider == 3) {
+            answerFound = true;
+            answer = testNumber;
+        }
+    }
+
+        testNumber += 20;
+}
+
+Console.WriteLine(answer);
+
