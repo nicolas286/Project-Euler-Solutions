14808843bee2e3f5c3d0a5b0190c5309518f802e
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-05 15:01:36 +0100

    Add files via upload

---
+++ Problem 6 Sum Square Difference.cs
@@ -0,0 +1,26 @@
+﻿
+// difference between sum of squares and square of sum is equal to 
+// the sum of double of the products : 
+//(a + b + c)² = a² + b² + c² + 2ab + 2bc + 2ca
+// thus the only number worth calculating is 2 * (1*2, 1*3...2*1, 2*3... 100)
+// (all products excluding squares)
+
+int result = 0;
+
+for (int a = 1; a <= 100; a++)
+{
+    for (int b = 1; b <= 100; b++)
+    {
+        if (b == a)
+        {
+            continue; // here we exclude squares
+        }
+        else
+        {
+            result += a * b;
+        }
+    }
+}
+
+Console.WriteLine(result);
+Console.ReadKey();
