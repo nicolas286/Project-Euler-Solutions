c19d6a2ad50a6624b8cd5a98c5b7ff2011683059
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-05 15:02:30 +0100

    Add files via upload

---
+++ Euler Problem 7 10001st square.cs
@@ -0,0 +1,34 @@
+﻿
+int primesFound = 1; // set at 1 to account for number 2
+int i = 0;
+long answer = 0;
+bool isPrime = true;
+
+while (primesFound <= 10001)
+{
+
+  long numberTested = (2 * i) + 1; // only tests odd numbers
+
+  for (int divider = 2; divider <= (int)Math.Sqrt(numberTested); divider++)
+  {
+    if ((numberTested % divider) == 0)
+    {
+      isPrime = false;
+      break;
+    }
+    if (divider == (int)Math.Sqrt(numberTested))
+    {
+      isPrime = true;
+    }
+  }
+
+  if (isPrime == true) {
+    primesFound++;
+  }
+
+  i++; 
+  answer = numberTested;
+}
+
+Console.WriteLine(answer);
+Console.ReadKey();
