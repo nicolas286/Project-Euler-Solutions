0fad358237ddc0e4444cc626428091384cce9fc6
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-06 19:32:07 +0100

    Add files via upload

---
+++ Problem 9 Special Pythagorean Triplet.cs
@@ -0,0 +1,24 @@
+﻿
+int a = 0;
+int b = 0;
+int c = 0;
+
+for (a = 0; a <= 500; a++)
+
+{
+    for (b = a+1; b <= 500; b++)
+    {
+        for (c = b+1; c <= 500; c++)
+        {
+            if ( a + b + c == 1000 && ( a * a ) + ( b * b ) == ( c * c) )
+            {
+                Console.WriteLine(a * b * c); 
+            }
+
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
