47dec9f0e699a74f05aa4658833c862e6aa04f91
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-07 13:27:08 +0100

    Add files via upload

---
+++ Problem 10 Summation of Primes.cs
@@ -0,0 +1,32 @@
+﻿
+long sum = 0;
+
+for (int i = 1; i <= 2000000; i += 1)
+{
+
+    if (i == 2 || i == 3)
+    {
+        sum += i;
+        continue;
+    }
+
+    for (int divisor = 2; divisor <= (int)Math.Sqrt(i); divisor += 1)
+    {
+        if (i == divisor)
+        {
+        continue;
+        }
+
+        else if (i % divisor == 0)
+        {
+        break;
+        }
+        
+        else if (divisor == (int)Math.Sqrt(i))
+        {
+        sum += i;
+        }
+    }
+}
+
+Console.WriteLine(sum);
