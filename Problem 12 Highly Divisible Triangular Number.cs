bac2ba3b6591adaee5d7e5e8cdb79f3f985b4d0e
Nico <155650119+nicolas286@users.noreply.github.com>
2024-02-09 21:41:07 +0100

    Add files via upload

---
+++ Problem 12 Highly Divisible Triangular Number.cs
@@ -0,0 +1,34 @@
+﻿
+int triangleNumber = 1;
+int counter = 2;
+int divisorCount = 0;
+bool answerFound = false;
+
+while (answerFound == false)
+{
+  
+  divisorCount = 0;
+
+  triangleNumber += counter;
+
+  for (int divisor = 1; divisor <= (int)Math.Sqrt(triangleNumber); divisor++)
+  {
+   if (triangleNumber % divisor == 0)
+   {
+      divisorCount += 2;
+   }
+  }
+
+  if (divisorCount > 500)
+  {
+   answerFound = true;
+  }
+
+  Console.WriteLine(divisorCount);
+
+  counter++;
+
+}
+
+Console.WriteLine(triangleNumber);
+
